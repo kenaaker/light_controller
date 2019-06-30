d426ec95e929df4bacd010f0a28791a8baf10aeb
Ken Aaker <kenaaker@gmail.com>
2019-06-29 21:22:33 -0500

    Consolidate all light on/off decisions.

--- light_state_determiner.cpp
+++ light_state_determiner.cpp
@@ -1,6 +1,82 @@
 #include "light_state_determiner.h"
 
-light_state_determiner::light_state_determiner()
-{
+light_state_determiner::light_state_determiner() {
+    light_intensity_value = 0;
+    last_time_occupancy_sensor_fired = QTime(0, 0, 0, 0); /* Start with sense having never fired */
+    occupancy_timeout_delta = QTime(0, 5, 0, 0);    /* Use 5 minutes as timeout value default */
+    user_light_setting = off;
+}
 
+void light_state_determiner::set_light_intensity(unsigned int light_in) {
+    light_intensity_value = light_in;
+}
+
+unsigned int light_state_determiner::light_intensity(void) {
+    return light_intensity_value;
+}
+
+void light_state_determiner::occupancy_sensor_fired(void) {
+    last_time_occupancy_sensor_fired = QTime::currentTime();
+}
+
+QTime light_state_determiner::last_occupancy_detected(void) {
+    return last_time_occupancy_sensor_fired;
+}
+
+void light_state_determiner::occupancy_detected(void) {
+    occupancy_sensor_fired();
+}
+
+void light_state_determiner::set_occupancy_timeout_delta(QTime &timeout_value) {
+    occupancy_timeout_delta = timeout_value;
+}
+
+light_states light_state_determiner::light_should_be(void) {
+    light_states ret_value;
+    if (user_light_setting == on) {
+        ret_value = on;
+    } else if (user_light_setting == off) {
+        ret_value = off;
+    } else { /* The user hasn't done anything particular yet, undefined state */
+        QTime occupancy_timeout;
+        unsigned int seconds_to_timeout;
+
+        seconds_to_timeout = occupancy_timeout_delta.hour() * 3600 +
+                occupancy_timeout_delta.minute() * 60 +
+                occupancy_timeout_delta.second();
+        occupancy_timeout = last_occupancy_detected().addSecs(seconds_to_timeout);
+        if (occupancy_timeout < QTime::currentTime()) {
+            ret_value = off;
+        } else {
+            /* Now check the light intensity to see if we should turn the light on. */
+            if (light_intensity_value > turn_on_low_bound) {
+                ret_value = on;
+            } else {
+                if (light_intensity_value < turn_off_high_bound) {
+                    ret_value = off;
+                }
+            }
+        }
+    }
+    return ret_value;
+}
+
+void light_state_determiner::user_set_light_off(void) {
+    user_light_setting = off;
+}
+
+light_states light_state_determiner::user_set_light_state(void) {
+    return user_light_setting;
+}
+
+void light_state_determiner::user_set_light_on(void) {
+    user_light_setting = on;
+}
+
+void light_state_determiner::set_turn_on_low_bound(unsigned int new_low_bound) {
+    turn_on_low_bound = new_low_bound;
+}
+
+void light_state_determiner::set_turn_off_high_bound(unsigned int new_high_bound) {
+    turn_off_high_bound = new_high_bound;
 }
--- light_state_determiner.h
+++ light_state_determiner.h
@@ -1,11 +1,33 @@
 #ifndef LIGHT_STATE_DETERMINER_H
 #define LIGHT_STATE_DETERMINER_H
 
+#include <QTime>
 
-class light_state_determiner
-{
-public:
+enum light_states { off, on, undefined };
+
+class light_state_determiner {
+  public:
     light_state_determiner();
+    void set_light_intensity(unsigned int light_in);
+    unsigned int light_intensity(void);
+    void set_turn_on_low_bound(unsigned int new_low_bound);
+    void set_turn_off_high_bound(unsigned int new_high_bound);
+    void occupancy_sensor_fired(void);
+    QTime last_occupancy_detected(void);
+    void occupancy_detected(void);
+    void set_occupancy_timeout_delta(QTime &timeout_value);
+    light_states light_should_be(void);
+    void user_set_light_off(void);
+    void user_set_light_on(void);
+    light_states user_set_light_state(void);
+
+  private:
+    unsigned int light_intensity_value;
+    QTime last_time_occupancy_sensor_fired;
+    QTime occupancy_timeout_delta;
+    enum light_states user_light_setting;
+    unsigned int turn_on_low_bound;
+    unsigned int turn_off_high_bound;
 };
 
 #endif // LIGHT_STATE_DETERMINER_H
